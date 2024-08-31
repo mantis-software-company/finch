83a9ab48df1f894a66120bfe293f350be61e5bcc
Furkan Kalkan <furkankalkan@mantis.com.tr>
2024-08-31 23:21:51 +0300

    fix: change windows dist filename for version

--- scripts/build_msi.bat
+++ scripts/build_msi.bat
@@ -1,3 +1,6 @@
+
+set "target_version=%~1"
+
 REM Include win32ctypes and keyring backend modules manually
 cxfreeze build_exe --includes=keyring.backends.SecretService,keyring.backends.libsecret,keyring.backends.chainer,keyring.backends.Windows,keyring.backends.kwallet,keyring.backends.macOS,win32ctypes.core.ctypes._authentication,win32ctypes.core.ctypes._common,win32ctypes.core.ctypes._dll,win32ctypes.core.ctypes._nl_support,win32ctypes.core.ctypes._resource,win32ctypes.core.ctypes._system_information,win32ctypes.core.ctypes._time,win32ctypes.core.ctypes._util
 
@@ -6,4 +9,4 @@ copy "build\exe.win-amd64-3.12\lib\PyQt5\Qt5\plugins\iconengines\Qt5Svg.dll" "bu
 
 REM Build MSI
 cxfreeze bdist_msi --skip-build
-move dist\*.msi "dist\finch-main-windows-x86_64.msi"
+move dist\*.msi "dist\finch-%target_version%-windows-x86_64.msi"
