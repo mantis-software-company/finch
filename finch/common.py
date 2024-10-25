fbaee336ca707f2e715ca1cac3592c2c1e5109a7
Furkan Kalkan <furkankalkan@mantis.com.tr>
2024-10-25 17:48:33 +0300

    fix: Use light theme by default in Windows due color incompatibilities.

--- finch/common.py
+++ finch/common.py
@@ -19,26 +19,28 @@ DATETIME_FORMAT = "%d %b %Y %H:%M"
 
 def apply_theme(app):
     """ Apply Dark Theme """
-    palette = QPalette()
-    palette.setColor(QPalette.Window, QColor(53, 53, 53))
-    palette.setColor(QPalette.WindowText, Qt.white)
-    palette.setColor(QPalette.Base, QColor(35, 35, 35))
-    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-    palette.setColor(QPalette.ToolTipBase, QColor(25, 25, 25))
-    palette.setColor(QPalette.ToolTipText, Qt.white)
-    palette.setColor(QPalette.Text, Qt.white)
-    palette.setColor(QPalette.Button, QColor(53, 53, 53))
-    palette.setColor(QPalette.ButtonText, Qt.white)
-    palette.setColor(QPalette.BrightText, Qt.red)
-    palette.setColor(QPalette.Link, QColor(42, 130, 218))
-    palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-    palette.setColor(QPalette.HighlightedText, QColor(35, 35, 35))
-    palette.setColor(QPalette.Active, QPalette.Button, QColor(53, 53, 53))
-    palette.setColor(QPalette.Disabled, QPalette.ButtonText, Qt.darkGray)
-    palette.setColor(QPalette.Disabled, QPalette.WindowText, Qt.darkGray)
-    palette.setColor(QPalette.Disabled, QPalette.Text, Qt.darkGray)
-    palette.setColor(QPalette.Disabled, QPalette.Light, QColor(53, 53, 53))
-    app.setPalette(palette)
+    # Use light theme by default in Windows due color incompatibilities.
+    if sys.platform != "win32":
+        palette = QPalette()
+        palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        palette.setColor(QPalette.WindowText, Qt.white)
+        palette.setColor(QPalette.Base, QColor(35, 35, 35))
+        palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        palette.setColor(QPalette.ToolTipBase, QColor(25, 25, 25))
+        palette.setColor(QPalette.ToolTipText, Qt.white)
+        palette.setColor(QPalette.Text, Qt.white)
+        palette.setColor(QPalette.Button, QColor(53, 53, 53))
+        palette.setColor(QPalette.ButtonText, Qt.white)
+        palette.setColor(QPalette.BrightText, Qt.red)
+        palette.setColor(QPalette.Link, QColor(42, 130, 218))
+        palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+        palette.setColor(QPalette.HighlightedText, QColor(35, 35, 35))
+        palette.setColor(QPalette.Active, QPalette.Button, QColor(53, 53, 53))
+        palette.setColor(QPalette.Disabled, QPalette.ButtonText, Qt.darkGray)
+        palette.setColor(QPalette.Disabled, QPalette.WindowText, Qt.darkGray)
+        palette.setColor(QPalette.Disabled, QPalette.Text, Qt.darkGray)
+        palette.setColor(QPalette.Disabled, QPalette.Light, QColor(53, 53, 53))
+        app.setPalette(palette)
 
 
 def center_window(self):
