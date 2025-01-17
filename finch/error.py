dd4f2dfe2d4949a7bb6ed230eaea8b8355add75a
furkankalkan <furkankalkan@mantis.com.tr>
2025-01-17 19:31:50 +0300

    improv: Add extra info to error dialog

--- finch/error.py
+++ finch/error.py
@@ -10,21 +10,29 @@ class ErrorDialog(QMessageBox):
         self.show_traceback = None
         self.setWindowTitle("Error")
         self.setIcon(QMessageBox.Critical)
+        self.extra_info = None
 
     def exec_(self):
         if self.show_traceback:
-            self.setDetailedText(traceback.format_exc())
+            detailed_text = traceback.format_exc()
+            if self.extra_info:
+                detailed_text += f"\n\nExtra Info:\n{self.extra_info}"
+            self.setDetailedText(detailed_text)
         super().exec_()
 
     def setShowTraceback(self, show_traceback):
         self.show_traceback = show_traceback
 
+    def setExtraInfo(self, extra_info):
+        self.extra_info = extra_info
 
-def show_error_dialog(error: Union[Exception, str], show_traceback=False):
+
+def show_error_dialog(error: Union[Exception, str], show_traceback=False, extra_info=None) -> None:
     msg = ErrorDialog()
     if isinstance(error, Exception):
         msg.setText(str(error))
     elif isinstance(error, str):
         msg.setText(error)
     msg.setShowTraceback(show_traceback)
+    msg.setExtraInfo(extra_info)
     msg.exec_()
